7a5681537deeace58c22e1d00e4f5d77594af648
bunnyUFO <danieldlgrzz@gmail.com>
2023-02-02 20:07:45 -0800

    animation to the beat example

--- Assets/Scripts/RythmFramework/BeatEventListener.cs
+++ Assets/Scripts/RythmFramework/BeatEventListener.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using FMOD.Studio;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RythmFramework
 {
     public class BeatEventListener : MonoBehaviour
     {
         public bool beat;
+        public UnityEvent beatAction;
+        
         public List<string> markers = new List<string>();
+        public List<UnityEvent> markerActions = new List<UnityEvent>();
 
         private void Start()
         {
@@ -20,12 +24,15 @@ namespace RythmFramework
         public void OnBeat()
         {
             Debug.Log("It's a beat event!");
+            if(beat) beatAction.Invoke();
         }
         
         public void OnMarker(string markerName)
         {
             if (markers.Contains(markerName))
             {
+                int markerIndex = markers.IndexOf(markerName);
+                if(markerIndex > -1) markerActions[markerIndex].Invoke();
                 Debug.Log($"It's a {markerName} marker event!");
             }
         }
