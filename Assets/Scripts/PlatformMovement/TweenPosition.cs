7d4847a7f47b5a7ff0450fc7a687d2eb3d42db1d
bunnyUFO <danieldlgrzz@gmail.com>
2023-02-03 01:43:19 -0800

    add scripts to animate splines   - tween position with optional delay   - interpolate spline using transforms   - set position of object relative to spline

---
+++ Assets/Scripts/PlatformMovement/TweenPosition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Misc;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace PlatformMovement
+{
+    public class TweenPosition : MonoBehaviour
+    {
+        public List<Transform> positions = new List<Transform>();
+        public float duration = 1;
+        public int currentPosition = 0;
+        
+        private LateExecute _lateExecute;
+
+        private void Awake()
+        {
+            _lateExecute = new LateExecute(this);
+        }
+
+        public void TweenNext()
+        {
+            currentPosition = (currentPosition + 1) % positions.Count;
+            transform.DOMove(positions[currentPosition].position, duration);
+        }
+        
+        public void TweenNext(float delay)
+        {
+            _lateExecute.DelayExecute(delay , x=> TweenNext());
+        }
+    }
+}
---
+++ Assets/Scripts/SplineMeshExtensions/SetPositionToSpline.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using SplineMesh;
+
+namespace SplineMeshExtensions
+{ 
+    [ExecuteInEditMode]
+    public class SetPositionToSpline  : MonoBehaviour
+    {
+        public Spline spline;
+        [Range(0, 1)]
+        public float rate;
+
+        private void Update()
+        {
+            CalculatePosition();
+        }
+        
+        void EditorUpdate()
+        {
+            CalculatePosition();
+        }
+
+        private void OnEnable()
+        {
+#if UNITY_EDITOR
+            EditorApplication.update += EditorUpdate;
+#endif
+        }
+
+        void OnDisable()
+        {
+#if UNITY_EDITOR
+            EditorApplication.update -= EditorUpdate;
+#endif
+        }
+
+        private void CalculatePosition()
+        {
+            if(spline is null) return; 
+            transform.position = spline.transform.position +  spline.GetSampleAtDistance(rate*spline.Length).location;
+        }
+    }
+}
