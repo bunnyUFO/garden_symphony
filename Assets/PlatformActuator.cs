f5fd7b5a80eda60e334d064164381765334045ea
bunnyUFO <danieldlgrzz@gmail.com>
2023-02-05 21:46:54 -0800

    Performance Fixes and move to URP

--- Assets/PlatformActuator.cs
+++ Assets/PlatformActuator.cs
@@ -2,17 +2,25 @@ using System;
 using System.Collections.Generic;
 using BehaviorDesigner.Runtime.Tasks.Unity.UnityGameObject;
 using RythmFramework;
+using SplineMesh;
+using SplineMeshExtensions;
 using UnityEngine;
 
 public class PlatformActuator : MonoBehaviour
 {
     public float enableDistance;
     private BeatEventListener[] listeners;
+    private SplineMeshTiling[] tilings;
+    private SplineSmoother[] smoothers;
+    private ExampleTentacle[] roots;
     private Transform player;
 
     private void Start()
     {
         listeners = GetComponentsInChildren<BeatEventListener>();
+        tilings = GetComponentsInChildren<SplineMeshTiling>();
+        smoothers = GetComponentsInChildren<SplineSmoother>();
+        roots = GetComponentsInChildren<ExampleTentacle>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
@@ -24,12 +32,36 @@ public class PlatformActuator : MonoBehaviour
             {
                 beatEventListener.enabled = false;
             }
+            foreach (var tiling in tilings)
+            {
+                tiling.enabled = false;
+            }
+            foreach (var smoother in smoothers)
+            {
+                smoother.enabled = false;
+            }
+            foreach (var root in roots)
+            {
+                root.enabled = false;
+            }
         }
         else
         {
             foreach (var beatEventListener in listeners)
             {
                 beatEventListener.enabled = true;
+            }
+            foreach (var tiling in tilings)
+            {
+                tiling.enabled = true;
+            }
+            foreach (var smoother in smoothers)
+            {
+                smoother.enabled = true;
+            }
+            foreach (var root in roots)
+            {
+                root.enabled = true;
             }
         }
     }
