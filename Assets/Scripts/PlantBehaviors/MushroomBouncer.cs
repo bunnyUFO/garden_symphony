fd92e2b18d167bc224809bd3b9216c603927937e
Danny Nguyen <67300046+duc55@users.noreply.github.com>
2023-02-05 19:51:20 -0800

    added option for Mushroom is always bouncy or only on beat

--- Assets/Scripts/PlantBehaviors/MushroomBouncer.cs
+++ Assets/Scripts/PlantBehaviors/MushroomBouncer.cs
@@ -4,31 +4,41 @@ using UnityEngine;
 
 public class MushroomBouncer : MonoBehaviour
 {
+    [SerializeField] bool bounceOnBeat;
     [SerializeField] float launchStrength;
     List<HeroStateMachine> heroesOnMushroom = new List<HeroStateMachine>();
 
 
     public void Launch()
     {
-        foreach (HeroStateMachine hero in heroesOnMushroom) {
-            Vector3 launchForce = Vector3.up * launchStrength;
-            
-            // hero.ForceReceiver.Jump(hero.JumpForce);
-            hero.SwitchState(new HeroJumpingState(hero));
-            hero.ForceReceiver.AddForce(launchForce);
+        if (! bounceOnBeat) return;
+
+        foreach (HeroStateMachine hero in heroesOnMushroom) {         
+            BounceHero(hero);
         }
+    }
+
+    private void BounceHero(HeroStateMachine hero)
+    {
+        Vector3 launchForce = transform.forward * launchStrength;
+        hero.SwitchState(new HeroJumpingState(hero));
+        hero.ForceReceiver.AddForce(launchForce);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) {
-            heroesOnMushroom.Add(other.GetComponent<HeroStateMachine>());
+            if (bounceOnBeat) {
+                heroesOnMushroom.Add(other.GetComponent<HeroStateMachine>());
+            } else {
+                BounceHero(other.GetComponent<HeroStateMachine>());
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")) {
+        if (other.CompareTag("Player") && bounceOnBeat) {
             heroesOnMushroom.Remove(other.GetComponent<HeroStateMachine>());
         }
     }
